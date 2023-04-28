0c324dc81ef52fc36203420b20cd9839422f955a
Meir-Moore <45730980+Meir-Moore@users.noreply.github.com>
2023-04-28 17:47:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,73 +4,86 @@ from texttable import Texttable
 from Players import Player
 from tabulate import tabulate
 
-name = input("""Welcome to War card game!
-Please enter your name: """)
 
-while not name.isalpha():
-    name = input("Please enter letters: ")
+# Get the player's name
+def get_player_name():
+    name = input("""Welcome to War card game!
+    Please enter your name: """)
 
-player = Player()
-computer = Player()
-player.name = name
-computer.name = "Jarvis"
+    while not name.isalpha():
+        name = input("Please enter letters: ")
 
-
-def game():
-    card_deck()
-    table_game()
-    while True:
-        try:
-            hit_card = int(input("Type 1 to hit a card, type 2 for exit: "))
-            if hit_card == 1:
-                play_round()
-            elif hit_card == 2:
-                print("Bye bye :)")
-                break
-            else:
-                print("Error, type again!")
-            time.sleep(0.6)
-            table_game()
-            time.sleep(0.6)
-            if computer.deck < 1:
-                player.win_game()
-                exit(0)
-            elif player.deck < 1:
-                computer.win_game()
-                exit(0)
-        except ValueError:
-            print("Invalid key")
+    return name
 
 
-def card_deck():
+# Create player and computer instances
+def create_players(name):
+    player = Player()
+    computer = Player()
+    player.name = name
+    computer.name = "Jarvis"
+    return player, computer
+
+
+# Function to create and shuffle the card deck
+def card_deck(player, computer):
     deck = pydealer.Deck()
     hand = pydealer.Stack()
 
     hand.add(deck.deal(52))
-
     hand.shuffle()
-    pack = []
-    for c in range(52):
-        pack.append(hand[c])
 
-    for c in range(26):
+    pack = list(hand)
+
+    # Deal cards to both players
+    for _ in range(26):
         player.add_card(pack.pop())
         computer.add_card(pack.pop())
 
 
-def play_round():
+# Function to display the current game table with player and computer decks
+def table_game(player, computer):
+    table_data = [["Name", "Deck"], [player.name, player.deck], [computer.name, computer.deck]]
+    table = Texttable()
+    table.add_rows(table_data)
+    print(table.draw())
+
+
+# Function to show the current cards being played
+def show_cards(player, computer):
+    text = f"{player.name} card: {player.card}"
+    text2 = f"{computer.name} card : {computer.card}"
+    table = [[text, text2]]
+    output = tabulate(table, tablefmt='fancy_grid')
+    print(output)
+
+
+# Function to announce a war
+def war_announce():
+    text = "War!"
+    table = [[text]]
+    output = tabulate(table, tablefmt='fancy_grid')
+    print(output)
+
+
+# Function to play a round of War
+def play_round(player, computer):
     player.hit_card()
     time.sleep(0.5)
     computer.hit_card()
     time.sleep(0.5)
-    show_cards()
+    show_cards(player, computer)
+
+    # Determine the winner of the round
     if player.card > computer.card:
         player.add_card(computer.card)
         player.add_card(player.card)
         time.sleep(0.5)
         player.win_round()
+
+        # Check if computer's deck is empty
         if computer.deck < 1:
-            table_game()
+            table_game(player, computer)
             time.sleep(0.5)
             player.win_game()
             exit(0)
@@ -79,19 +92,25 @@ def play_round():
         computer.add_card(computer.card)
         time.sleep(0.5)
         computer.win_round()
+
+        # Check if player's deck is empty
         if player.deck < 1:
-            table_game()
+            table_game(player, computer)
             time.sleep(0.5)
             computer.win_game()
             exit(0)
     elif player.card == computer.card:
         player.add_war_card(player.card)
         computer.add_war_card(computer.card)
+
+        # Continue to play a war until a winner is determined
         while True:
             war_announce()
             time.sleep(0.5)
-            war()
+            war(player, computer)
             time.sleep(0.5)
+
+            # Determine the winner of the war
             if player.card > computer.card:
                 for c in computer.war_deck:
                     player.add_war_card(c)
@@ -101,7 +120,8 @@ def play_round():
                 player.win_war()
                 computer.lose_war()
                 break
-            elif player.card < computer.card:
+            elif player
+                        card < computer.card:
                 for c in player.war_deck:
                     computer.add_war_card(c)
                 for c in computer.war_deck:
@@ -112,9 +132,11 @@ def play_round():
                 break
 
 
-def war():
+# Function to play a war
+def war(player, computer):
+    # If player's deck has 3 or fewer cards
     if player.deck <= 3:
-        for count in range(player.deck):
+        for _ in range(player.deck):
             time.sleep(0.5)
             player.hit_card()
             player.add_war_card(player.card)
@@ -122,9 +144,10 @@ def war():
             computer.hit_card()
             computer.add_war_card(computer.card)
         time.sleep(0.5)
-        show_cards()
+        show_cards(player, computer)
+    # If computer's deck has 3 or fewer cards
     elif computer.deck <= 3:
-        for count in range(computer.deck):
+        for _ in range(computer.deck):
             time.sleep(0.5)
             computer.hit_card()
             computer.add_war_card(computer.card)
@@ -132,9 +155,10 @@ def war():
             player.hit_card()
             player.add_war_card(player.card)
         time.sleep(0.5)
-        show_cards()
+        show_cards(player, computer)
+    # If both players have more than 3 cards
     else:
-        for count in range(2):
+        for _ in range(2):
             time.sleep(0.5)
             player.hit_card()
             player.add_war_card(player.card)
@@ -148,29 +172,40 @@ def war():
         computer.hit_card()
         computer.add_war_card(computer.card)
         time.sleep(0.5)
-        show_cards()
+        show_cards(player, computer)
 
 
-def show_cards():
-    text = f"{player.name} card: {player.card}"
-    text2 = f"{computer.name} card : {computer.card}"
-    table = [[text, text2]]
-    output = tabulate(table, tablefmt='fancy_grid')
-    print(output)
+def game():
+    player_name = get_player_name()
+    player, computer = create_players(player_name)
+    card_deck(player, computer)
+    table_game(player, computer)
+
+    # Main game loop
+    while True:
+        try:
+            hit_card = int(input("Type 1 to hit a card, type 2 for exit: "))
+            if hit_card == 1:
+                play_round(player, computer)
+            elif hit_card == 2:
+                print("Bye bye :)")
+                break
+            else:
+                print("Error, type again!")
+            time.sleep(0.6)
+            table_game(player, computer)
+            time.sleep(0.6)
+
+            # Check if either player's deck is empty and end the game
+            if computer.deck < 1:
+                player.win_game()
+                exit(0)
+            elif player.deck < 1:
+                computer.win_game()
+                exit(0)
+        except ValueError:
+            print("Invalid key")
 
 
-def table_game():
-    table_data = [["Name", "Deck"], [player.name, player.deck], [computer.name, computer.deck]]
-    table = Texttable()
-    table.add_rows(table_data)
-    print(table.draw())
-
-
-def war_announce():
-    text = "War!"
-    table = [[text]]
-    output = tabulate(table, tablefmt='fancy_grid')
-    print(output)
-
-
-game()
+if __name__ == "__main__":
+    game()
